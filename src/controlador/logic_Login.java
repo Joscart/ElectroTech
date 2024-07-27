9b155a29c5bdd354a8c4a190ec76577bd290c81d
Jose <jguallasaminc@est.ups.edu.ec>
2024-07-26 22:16:27 -0500

    vistas

--- src/controlador/logic_Login.java
+++ src/controlador/logic_Login.java
@@ -1,6 +1,8 @@
 package controlador;
 
 import vista.Login;
+import vista.Menu;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
@@ -24,8 +26,16 @@ public class logic_Login {
         String password = new String(loginView.passwordField.getPassword());
 
         if (username.equals("admin") && password.equals("12345")) {
+            loginView.textFieldUsername.setBackground(Color.GREEN);
+            loginView.passwordField.setBackground(Color.GREEN);
             JOptionPane.showMessageDialog(loginView, "Login exitoso!");
+            // Abrir ventana Menu y cerrar ventana Login
+            Menu menu = new Menu();
+            menu.setVisible(true);
+            loginView.dispose();
         } else {
+            loginView.textFieldUsername.setBackground(Color.RED);
+            loginView.passwordField.setBackground(Color.RED);
             JOptionPane.showMessageDialog(loginView, "Usuario o contraseña incorrectos.");
         }
     }
